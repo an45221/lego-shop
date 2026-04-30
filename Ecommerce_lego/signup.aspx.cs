d93188a254fa488bf5b233dfeb1255ebc1d644b3
Anurag Budha <155082249+an45221@users.noreply.github.com>
2026-04-30 14:40:11 +0545

    Insert Data into Database Table

--- Ecommerce_lego/signup.aspx.cs
+++ Ecommerce_lego/signup.aspx.cs
@@ -1,25 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Ecommerce_lego
 {
     public partial class signup : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection("data Source = (localdb)\\mssqllocaldb; database= lego; integrated Security = true");
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; database = lego; Integrated Security=True");
 
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO legoshop (FullName, Email, Password) VALUES(@FullName, @Email, @Password)", con);
 
+            cmd.Parameters.AddWithValue("@FullName", txtFullName.Text);
+            cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
